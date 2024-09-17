c18c49f361067d9c05cf26086af1dc9cf48293ef
Daniel Weijmers <daniel@weijmers.com>
2024-09-17 15:08:53 +0200

    adding simple lambda

---
+++ functions/Test.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.SystemTextJson;
+
+[assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
+namespace functions;
+
+public class Test
+{
+    public Test() { }
+
+    public async Task Handler(object input, ILambdaContext context)
+    {
+        Console.WriteLine("Testing testing ...");
+        await Task.CompletedTask;
+    }
+}
